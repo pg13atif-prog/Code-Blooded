f70f65a121849e9a11e1ff35ae1aece56c8dd5a4
Atif <pg13atif@gmail.com>
2026-08-11 00:49:44 +0530

    Revert authDomain to hackathon-backend-2b6c6.firebaseapp.com for valid Firebase OAuth handler resolution

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -7,7 +7,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCQtvZluQkqv1yZGHqZwIRgp_NYzqN3JEw",
-  authDomain: "cinescopeai.vercel.app",
+  authDomain: "hackathon-backend-2b6c6.firebaseapp.com",
   databaseURL: "https://hackathon-backend-2b6c6-default-rtdb.firebaseio.com",
   projectId: "hackathon-backend-2b6c6",
   storageBucket: "hackathon-backend-2b6c6.firebasestorage.app",
